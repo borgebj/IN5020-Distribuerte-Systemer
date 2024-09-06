e1e48fcd0650349065fe732ca176dff0bdeef15c
Børge Bjørnstadjordet <57920815+borgebj@users.noreply.github.com>
2024-09-06 14:24:52 +0200

    Add files via upload
    Lagt til oblig1 template

---
+++ oblig1/solution/src/main/java/com/ass1/Client/Client.java
@@ -0,0 +1,22 @@
+import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import com.ass1.Server.ServerInterface;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Registry registry = LocateRegistry.getRegistry();
+            ServerInterface server = (ServerInterface) registry.lookup("server");
+            System.out.println(server.Add(10, 20));
+        }
+        catch (RemoteException | NotBoundException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ oblig1/solution/src/main/java/com/ass1/Server/ServerInterface.java
@@ -0,0 +1,7 @@
+package com.ass1.Server;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServerInterface extends Remote {
+    int Add(int num1, int num2) throws RemoteException;
+}
