98b5f755b09c79eb1795c0a06b490429f6c43878
borgebj <borgetull@gmail.com>
2024-09-17 19:02:40 +0200

    endret noe

--- ass1/src/main/java/ass1/proxy/ProxyServer.java
+++ ass1/src/main/java/ass1/proxy/ProxyServer.java
@@ -2,7 +2,6 @@ package ass1.proxy;
 
 import ass1.server.Server;
 
-import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -17,7 +16,7 @@ public class ProxyServer implements ProxyClientInterface {
 	private HashMap<Integer, Server> servers;
 
 	// Hashmap keeping track of requests in each zone
-	private int WORKLOAD_THRESHOLD = 100;
+	private int WORKLOAD_THRESHOLD = 200;
 	private ConcurrentMap<Integer, Integer> zoneRequests;
 
 
@@ -81,7 +80,7 @@ public class ProxyServer implements ProxyClientInterface {
 
 		// 2. check request counter
 		int requests = zoneRequests.get(zone);
-		if (requests % WORKLOAD_THRESHOLD == 0)
+		if (requests % 18 == 0)
 		{
 			try {
 				// attempt to fetch server load
@@ -106,7 +105,7 @@ public class ProxyServer implements ProxyClientInterface {
 
 		Server server = servers.get(zone);
 
-		System.out.printf("\nRequesting zone:%d\n", zone);
+		System.out.printf("\nRequesting zone %d\n", zone);
 
 		// load of requested zone + how many servers in total
 		int requestedWorkload = zoneRequests.get(zone);
@@ -129,12 +128,14 @@ public class ProxyServer implements ProxyClientInterface {
 			// redirect to adjacent zone 1 if below threshold
 			if (adjacentWorkload1 < WORKLOAD_THRESHOLD) {
 				System.out.printf(CYAN + "Redirecting to zone:%d (Workload: %d)\n" + RESET, adjacent1, adjacentWorkload1);
+				System.exit(-1);
 				return adjacentServer1;
 			}
 
 			// redirect to adjacent zone 2 if below threshold
 			if (adjacentWorkload2 < WORKLOAD_THRESHOLD) {
 				System.out.printf(CYAN + "Redirecting to zone:%d (Workload: %d)\n" + RESET, adjacent2, adjacentWorkload2);
+				System.exit(-1);
 				return adjacentServer2;
 			}
 		}
