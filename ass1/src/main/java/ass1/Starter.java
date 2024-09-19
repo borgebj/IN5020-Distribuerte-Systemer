ecaf288ffd91cc885138f48a6198cc5335fc1a09
Børge Bjørnstadjordet <borgetull@gmail.com>
2024-09-19 17:41:10 +0200

    Endring i test-fil Starter

--- ass1/src/main/java/ass1/Starter.java
+++ ass1/src/main/java/ass1/Starter.java
@@ -5,9 +5,34 @@ import ass1.server.ServerSimulator;
 
 public class Starter {
 	public static void main(String[] args) throws InterruptedException {
-		String[] simArgs = new String[]{};
-		ServerSimulator.main(simArgs);
-		Thread.sleep(500);
-		ClientSimulator.main(simArgs);
+		// Create thread for ServerSimulator
+		Thread serverThread = new Thread(() -> {
+			try {
+				ServerSimulator.main(new String[]{});
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+
+
+
+		// Create thread for ClientSimulator
+		Thread clientThread = new Thread(() -> {
+			try {
+				// Add a delay to give the server some time to start
+				Thread.sleep(500);
+				ClientSimulator.main(new String[]{});
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+
+		// Start both threads
+		serverThread.start();
+		clientThread.start();
+
+		// Optionally join the threads to wait for them to complete
+		serverThread.join();
+		clientThread.join();
 	}
 }
